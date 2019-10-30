0e5365067072ff0fdc59a61e7b8e69e37eaa47dc
Ze Xiu <ze.xiu@ranplanwireless.com>
2019-10-30 08:02:08 +0800

    Create Communication project

---
+++ TrackGenius/TrackGenius.Communication/ISerialPortWrapper.cs
@@ -0,0 +1,18 @@
+﻿using System.IO.Ports;
+
+namespace TrackGenius.Communication
+{
+    public interface ISerialPortWrapper
+    {
+        SerialPort Port { get; }
+        string Name { get; }
+
+        int PortNumber { get; }
+
+        void CreatePort(string portName, int baudRate, Parity parity);
+
+        void OpenPort(int portNumber);
+
+        void ClosePort();
+    }
+}
---
+++ TrackGenius/TrackGenius.Communication/SerialPortWrapper.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO.Ports;
+using TrackGenius.Protocol;
+
+namespace TrackGenius.Communication
+{
+    public class SerialPortWrapper : ISerialPortWrapper
+    {
+        public SerialPort Port { get; private set; }
+        public string Name { get; }
+        public int PortNumber { get; }
+
+        public SerialPortWrapper(ISerialPortSettings settings)
+        {
+            
+        }
+
+        public void CreatePort(string portName, int baudRate, Parity parity)
+        {
+            Port = new SerialPort(portName, baudRate, parity);
+        }
+
+        public void OpenPort(int portNumber)
+        {
+            Port.Open();
+            
+        }
+
+        public void ClosePort()
+        {
+            Port.Close();
+        }
+    }
+}
