1e55d6a8767983094e248643ade451f3d8d60db1
Anakin Xiu <anakin.xiu@foxmail.com>
2020-01-29 17:07:56 +0800

    Add `RaceClub` model

---
+++ TrackGenius/TrackGenius.Model/Interfaces/IRaceClub.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace TrackGenius.Model.Interfaces
+{
+    interface IRaceClub
+    {
+        Guid ClubID { get; set; }
+
+        string ClubName { get; set; }
+    }
+}
---
+++ TrackGenius/TrackGenius.Model/RaceClub.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using TrackGenius.Model.Interfaces;
+
+namespace TrackGenius.Model
+{
+    public class RaceClub : IRaceClub
+    {
+        public Guid ClubID { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string ClubName { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+    }
+}
