83021287b6a5bad906502c0e2b61e0f7a6bb89ad
Anakin Xiu <anakin.xiu@foxmail.com>
2020-01-30 17:17:06 +0800

    Add extension class for byte array type used in data packet convertion.

---
+++ TrackGenius/TrackGenius.Const/ByteArrayExtension.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Core;
+using MoreLinq;
+
+namespace TrackGenius.Const
+{
+    public static class ByteArrayExtension
+    {
+        public static byte[] Cut(this byte[] byteArray, int index, int length)
+        {
+            if (index < 0 || index > byteArray.Length)
+                throw new ArgumentOutOfRangeException(nameof(index));
+
+            if (index + length > byteArray.Length)
+                throw new ArgumentOutOfRangeException(nameof(length));
+
+            var result = new byte[length];
+            byteArray.CopyTo(result, index);
+
+            return result;
+        }
+
+        public static byte[] Reverse(this byte[] byteArray)
+        {
+            var result = new byte[byteArray.Length];
+            byteArray.ForEach((b, index) => result[byteArray.Length - index] = b);
+
+            return result;
+        }
+
+        public static int ToInt32(this byte[] byteArray)
+        {
+            var result = 0;
+            byteArray.ForEach((b, index) => result += b << (byteArray.Length - index - 1) * 8);
+
+            return result;
+        }
+    }
+}
