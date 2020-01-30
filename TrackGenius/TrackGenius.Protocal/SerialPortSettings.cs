ae45d14ead0fe4ef420e7acf935beb7a190cbfc1
Anakin Xiu <anakin.xiu@foxmail.com>
2020-01-30 12:42:58 +0800

    Add implement of ISerialPortSettings

---
+++ TrackGenius/TrackGenius.Protocal/SerialPortSettings.cs
@@ -0,0 +1,23 @@
+﻿using System.IO.Ports;
+
+namespace TrackGenius.Protocol
+{
+    public class SerialPortSettings : ISerialPortSettings
+    {
+        public int BaudRate { get; private set; }
+
+        public StopBits StopBit { get; private set; }
+
+        public Parity Parity { get; private set; }
+
+        public int Length { get; private set; }
+
+        public SerialPortSettings(int baudRate, StopBits stopBits, Parity parity, int length)
+        {
+            BaudRate = baudRate;
+            StopBit = stopBits;
+            Parity = Parity;
+            Length = length;
+        }
+    }
+}
