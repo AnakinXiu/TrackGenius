f770c73ff7be3f03ac46f55a79ed03734c42e9c2
Anakin Xiu <anakin.xiu@foxmail.com>
2020-01-30 17:15:21 +0800

    Add test project for protocal project

---
+++ TrackGenius/TrackGenius.ProtocolTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TrackGenius.ProtocolTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TrackGenius.ProtocolTests")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("06421cfb-0f42-458c-b92f-0bb0200e736c")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ TrackGenius/TrackGenius.ProtocolTests/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿using NSubstitute;
+using NUnit.Framework;
+using TrackGenius.Protocol;
+
+namespace TrackGenius.ProtocolTests
+{
+    [TestFixture]
+    public class UnitTest1
+    {
+        [Test]
+        public void TestMethod1()
+        {
+            var uplinkMessage = Substitute.For<IUplinkMessage>();
+        }
+    }
+}
