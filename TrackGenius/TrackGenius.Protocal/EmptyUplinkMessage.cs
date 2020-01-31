dff14cd4ff40b0d6baf204124dfa2fc1feb06e94
Anakin Xiu <anakin.xiu@foxmail.com>
2020-01-31 18:21:23 +0800

    Use null object pattern in message decoder.

---
+++ TrackGenius/TrackGenius.Protocal/EmptyUplinkMessage.cs
@@ -0,0 +1,17 @@
+﻿using TrackGenius.Protocol.Robitronic;
+
+namespace TrackGenius.Protocol
+{
+    public class EmptyUplinkMessage : IUplinkMessage
+    {
+        public byte Checksum => 0;
+
+        public PacketType PacketType => PacketType.Unknown;
+
+        public int PacketLength => 0;
+
+        public byte[] ByteData => new byte[0];
+
+        public string Deserialize() => string.Empty;
+    }
+}
