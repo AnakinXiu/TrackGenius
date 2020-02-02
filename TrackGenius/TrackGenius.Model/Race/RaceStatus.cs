f79d7fa04a9c1925d0b47b8da66f5111fab42434
Anakin Xiu <anakin.xiu@foxmail.com>
2020-02-02 16:27:56 +0800

    Add `RaceStatus` class to record car racing status.

---
+++ TrackGenius/TrackGenius.Model/Race/RaceStatus.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace TrackGenius.Model
+{
+    public class RaceStatus
+    {
+        public IDriver Driver { get; }
+
+        public ICar Car { get; }
+
+        public int LapsCount { get; set; }
+
+        public TimeSpan RacedTime { get; set; }
+
+        public RaceStatus(IDriver driver, ICar car)
+        {
+            Driver = driver;
+            Car = car;
+        }
+    }
+}
