2ce5489ca7836c0496fe6c32cef37890c1745ab3
Anakin Xiu <anakin.xiu@foxmail.com>
2026-06-07 22:55:14 +0800

    Add race page content

---
+++ source/TrackGenius/ViewModels/RacePageViewModel.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+using TrackGenius.Const;
+
+namespace TrackGenius.UI.ViewModels;
+
+public class RacePageViewModel :INotifyPropertyChanged
+{
+    public event PropertyChangedEventHandler PropertyChanged;
+
+    public ObservableCollection<RaceDataItem> RaceDataItems { get; set; } = new ObservableCollection<RaceDataItem>();
+}
+
+public class RaceDataItem : INotifyPropertyChanged
+{
+    private int _racerNumber;
+    private int _racerPosition;
+    private int _lapsCount;
+    public event PropertyChangedEventHandler PropertyChanged;
+    
+    public string DriverName { get; set; }
+
+    public int RacerNumber
+    {
+        get => _racerNumber;
+        set => PropertyChanged.RaiseIfChanged(this, ref _racerNumber, value, nameof(RacerNumber));
+    }
+
+    public int RacerPosition
+    {
+        get => _racerPosition;
+        set => PropertyChanged.RaiseIfChanged(this, ref _racerPosition, value, nameof(RacerPosition));
+    }
+
+    public int LapsCount
+    {
+        get => _lapsCount;
+        set => PropertyChanged.RaiseIfChanged(this, ref _lapsCount, value, nameof(LapsCount));
+    }
+
+    public int LapsCount
+    {
+        get => _lapsCount;
+        set => PropertyChanged.RaiseIfChanged(this, ref _lapsCount, value, nameof(LapsCount));
+    }
+
+    public string Description { get; set; }
+}
